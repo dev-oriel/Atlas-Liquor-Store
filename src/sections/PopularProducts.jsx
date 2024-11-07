3737f7e521ada443ef44efeb662a61cb2854e62b
dev-oriel <kiplangatoriel@gmail.com>
2024-11-07 11:28:20 +0300

    Removed the search bar

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -36,26 +36,7 @@ const PopularProducts = () => {
         <p className="lg:max-w-lg mt-1 text-slate-gray text-center px-2 sm:px-0">
           Browse our top-notch quality and the best beers, wines, and accessories
         </p>
-        <div className="relative w-full max-w-md mt-4 px-2 sm:px-0">
-          <div className="relative flex items-center">
-            <span className="absolute left-3 text-gray-400">
-              <i className="fas fa-search"></i>
-            </span>
-            <input
-              type="text"
-              placeholder="Search products..."
-              value={searchQuery}
-              onChange={handleSearchChange}
-              className="w-full py-2 pl-10 pr-4 border border-gray-300 rounded-lg focus:outline-none focus:border-coral-red"
-            />
-            <button
-              onClick={handleSearchClick}
-              className="ml-2 px-4 py-2 bg-coral-red text-white rounded-lg hover:bg-coral-dark transition duration-200 ease-in-out"
-            >
-              Search
-            </button>
-          </div>
-        </div>
+       
       </div>
 
       {/* Horizontal scroll section for products */}
